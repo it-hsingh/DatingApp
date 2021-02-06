f9a825e8ca74a0a3bd568a2c59acbc9aaa46ae5b
Harish Singh <it_harish@hotmail.com>
2021-02-07 01:33:53 +0530

    Implemented Repository Pattern

--- API/Controllers/UsersConroller.cs
+++ API/Controllers/UsersConroller.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -18,13 +19,11 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
             return await _context.Users.ToListAsync();
         }
 
         [HttpGet]
-        [Authorize]
         [Route("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id){
             return await _context.Users.FindAsync(id);
