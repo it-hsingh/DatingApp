8ca930d001ba18a7454d0a353cd4aa8e3d473f0f
Harish Singh <it_harish@hotmail.com>
2021-02-11 02:06:03 +0530

    interceptor to add bearer token and design pages

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -12,7 +13,7 @@ export class AccountService {
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  baseUrl='https://localhost:5001/api/';
+  baseUrl= environment.apiUrl;
   constructor(private http:HttpClient) { }
   login(model:any){
     return this.http.post(this.baseUrl+'account/login',model).pipe(
